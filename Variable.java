726c864bf3ef1ffd16e805bcef08721c3903eedc
durgeshkumar18 <durgeshkumar18>
2026-07-19 15:52:52 +0530

    Add Variable.java - variable types demo

---
+++ Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String[] args) {
+        int age = 20;
+        double salary = 45000;
+        char grade = 'A';
+        boolean placed = false;
+        String name = "Durgesh Kumar Nishad";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Placed: " + placed);
+    }
+}
